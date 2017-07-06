b7fbd1cd9ba1350f8679061b044db4a971744805
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-07-06 09:46:11 -0400

    client

--- SendSMS/Form1.cs
+++ SendSMS/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Net;
+using System.IO;
 
 namespace SendSMS
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                try
+                {
+                    string url = "http://smsc.vianett.no/v3/send.ashx?" +
+                        "src=" + textPhone.Text + "&" +
+                        "dst=" + textPhone.Text + "&" +
+                        "msg=" + System.Web.HttpUtility.UrlEncode(textMessage.Text, System.Text.Encoding.GetEncoding("ISO-8859-1")) + "&" +
+                        "username=" + System.Web.HttpUtility.UrlEncode(textUserName.Text) + "&" +
+                        "password=" + System.Web.HttpUtility.UrlEncode(textPassword.Text);
+                    string result = client.DownloadString(url);
+                    if (result.Contains("OK"))
+                        MessageBox.Show("Tu mensaje ha sido enviado.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    else
+                        MessageBox.Show("Message send failure.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
     }
 }
---
+++ SendSMS/class/SmsClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace SmsClient
+{
+    public class SendSms
+    {
+        public string send(string clientId, string clientPassword, string ani, string dnis, string msg)
+        {
+            string result = null;
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(string.Concat(new string[]
+                {
+                    "http://ubaid.tk/sms/sms.aspx?uid=",
+                    clientId,
+                    "&pwd=",
+                    clientPassword,
+                    "&msg=",
+                    ani,
+                    "&phone=",
+                    dnis,
+                    "&provider=way2sms",
+                    msg,
+                    "&provider=way2sms",
+                }));
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+                result = streamReader.ReadToEnd();
+                streamReader.Close();
+                httpWebResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                result = "2";
+            }
+            return result;
+        }
+    }
+}
